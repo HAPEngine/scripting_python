8ed954adeb309937f00330060d2627f79310a411
Bailey Stoner <monokrome@monokro.me>
2017-07-04 01:43:44 -0700

    (split) Add simple Python interpretter module.

---
+++ src/scripting_python.c
@@ -0,0 +1,36 @@
+#include <Python.h>
+#include <hap.h>
+
+
+void* create(HAPEngine *engine) {
+	Py_SetProgramName(((wchar_t**) (*engine).argvp)[0]);
+	Py_Initialize();
+}
+
+
+void load(HAPEngine *engine, void *state, char *identifier) {
+	(void)engine;
+	(void)identifier;
+	(void)state;
+}
+
+
+HAPTime update(HAPEngine *engine, void *state) {
+	(void)engine;
+	(void)state;
+
+	return -1;
+}
+
+
+void unload(HAPEngine *engine, void *state) {
+	(void)engine;
+	(void)state;
+}
+
+
+void destroy(HAPEngine *engine, void *state) {
+	(void)state;
+
+	Py_Finalize();
+}
