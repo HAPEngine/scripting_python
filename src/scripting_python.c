712149a1ddea1eb05bbe80ff16aa92186716f548
Bailey Stoner <monokrome@monokro.me>
2017-07-09 16:28:20 -0700

    (split) Add pragma comments because Windows is a bad thing.

--- src/scripting_python.c
+++ src/scripting_python.c
@@ -1,6 +1,14 @@
+/** Modifies the linker so that it uses proper symbol names in MSVC. **/
+#ifdef OS_Windows
+#pragma comment(linker, "/EXPORT:create=create")
+#pragma comment(linker, "/EXPORT:load=load")
+#pragma comment(linker, "/EXPORT:update=update")
+#pragma comment(linker, "/EXPORT:unload=unload")
+#pragma comment(linker, "/EXPORT:destroy=destroy")
+#endif
+
 #include <Python.h>
 #include <hap.h>
-
 
 void* create(HAPEngine *engine) {
 	Py_SetProgramName(((wchar_t**) (*engine).argvp)[0]);
@@ -19,7 +27,7 @@ void load(HAPEngine *engine, void *state, char *identifier) {
 HAPTime update(HAPEngine *engine, void *state) {
 	(void)engine;
 	(void)state;
-
+	
 	return 0;
 }
 
